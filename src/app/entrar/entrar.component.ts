21b735e5911bc3ea79b751f605e39982e066939d
Pedro Silva <pg308758@gmail.com>
2021-07-02 16:55:36 -0300

    implementação dos métodos http get e post no component tema

--- src/app/entrar/entrar.component.ts
+++ src/app/entrar/entrar.component.ts
@@ -31,8 +31,6 @@ export class EntrarComponent implements OnInit {
       environment.nome = this.userLogin.nome
       environment.foto = this.userLogin.foto
       environment.id = this.userLogin.id
-      
-      console.log(environment.token)
 
 
 
--- src/app/service/auth.service.ts
+++ src/app/service/auth.service.ts
@@ -15,13 +15,16 @@ export class AuthService {
   ) { }
 
   entrar(userLogin: UserLogin) : Observable<UserLogin>{
+    return this.http.post<UserLogin>('http://localhost:8080/usuarios/logar' , userLogin)
     //return this.http.post<UserLogin>('https://blogpedroatualizado.herokuapp.com/usuarios/logar', userLogin)
-    return this.http.post<UserLogin>('https://backendthiagofaccipieri.herokuapp.com/usuarios/logar', userLogin)
+    //return this.http.post<UserLogin>('https://backendthiagofaccipieri.herokuapp.com/usuarios/logar', userLogin)
 
   }
   cadastrar(usuario: Usuario) : Observable<Usuario>{
+    
+    return this.http.post<Usuario>('http://localhost:8080/usuarios/cadastrar', usuario)
     //return this.http.post<Usuario>('https://blogpedroatualizado.herokuapp.com/usuarios/cadastrar', usuario)
-    return this.http.post<Usuario>('https://backendthiagofaccipieri.herokuapp.com/usuarios/cadastrar', usuario)
+    //return this.http.post<Usuario>('https://backendthiagofaccipieri.herokuapp.com/usuarios/cadastrar', usuario)
 
 
   }
