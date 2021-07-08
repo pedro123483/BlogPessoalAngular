9c4d2f78a8d84e5ec0c1a7273103b0afcb6621ce
Pedro Silva <pg308758@gmail.com>
2021-07-07 22:30:41 -0300

    implementação do post e get de postagens

--- src/app/service/auth.service.ts
+++ src/app/service/auth.service.ts
@@ -29,6 +29,10 @@ export class AuthService {
 
   }
 
+  getByIdUser(id: number): Observable<Usuario>{
+    return this.http.get<Usuario>(`http://localhost:8080/usuarios/${id}`)
+  }
+
   logado(){
     let ok: boolean = false
 
