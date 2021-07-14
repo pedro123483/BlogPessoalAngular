86406ad42d8146c0b0fa13b43386018403504f07
Pedro Silva <pg308758@gmail.com>
2021-07-14 18:01:08 -0300

    blog finalizado

--- src/app/service/auth.service.ts
+++ src/app/service/auth.service.ts
@@ -15,14 +15,14 @@ export class AuthService {
   ) { }
 
   entrar(userLogin: UserLogin) : Observable<UserLogin>{
-    return this.http.post<UserLogin>('http://localhost:8080/usuarios/logar' , userLogin)
-    //return this.http.post<UserLogin>('https://blogpedroatualizado.herokuapp.com/usuarios/logar', userLogin)
+    return this.http.post<UserLogin>('https://blogpessoalpedro.herokuapp.com/usuarios/logar', userLogin)
+    //return this.http.post<UserLogin>('http://localhost:8080/usuarios/logar' , userLogin)
     //return this.http.post<UserLogin>('https://backendthiagofaccipieri.herokuapp.com/usuarios/logar', userLogin)
 
   }
   cadastrar(usuario: Usuario) : Observable<Usuario>{
-    
-    return this.http.post<Usuario>('http://localhost:8080/usuarios/cadastrar', usuario)
+    return this.http.post<Usuario>('https://blogpessoalpedro.herokuapp.com/usuarios/cadastrar', usuario)
+    //return this.http.post<Usuario>('http://localhost:8080/usuarios/cadastrar', usuario)
     //return this.http.post<Usuario>('https://blogpedroatualizado.herokuapp.com/usuarios/cadastrar', usuario)
     //return this.http.post<Usuario>('https://backendthiagofaccipieri.herokuapp.com/usuarios/cadastrar', usuario)
 
--- src/app/service/tema.service.ts
+++ src/app/service/tema.service.ts
@@ -16,29 +16,29 @@ export class TemaService {
   }
 
   getAllTema(): Observable<Tema[]>{
-    return this.http.get<Tema[]>('http://localhost:8080/tema', this.token)
+    return this.http.get<Tema[]>('https://blogpessoalpedro.herokuapp.com/tema', this.token)
 
   }
 
   getByIdTema(id: number): Observable<Tema>{
-    return this.http.get<Tema>(`http://localhost:8080/tema/${id}`, this.token)
+    return this.http.get<Tema>(`https://blogpessoalpedro.herokuapp.com/tema/${id}`, this.token)
   }
 
   getByNomeTema(nome: string): Observable<Tema[]>{
-    return this.http.get<Tema[]>(`http://localhost:8080/tema/nome/${nome}`, this.token)
+    return this.http.get<Tema[]>(`https://blogpessoalpedro.herokuapp.com/tema/nome/${nome}`, this.token)
   }
 
   postTema(tema: Tema): Observable<Tema>{
-    return this.http.post<Tema>('http://localhost:8080/tema', tema, this.token)
+    return this.http.post<Tema>('https://blogpessoalpedro.herokuapp.com/tema', tema, this.token)
 
   }
 
   putTema(tema: Tema): Observable<Tema>{
-    return this.http.put<Tema>('http://localhost:8080/tema', tema, this.token)
+    return this.http.put<Tema>('https://blogpessoalpedro.herokuapp.com/tema', tema, this.token)
   }
 
   deleteTema(id: number){
-    return this.http.delete(`http://localhost:8080/tema/${id}`, this.token)
+    return this.http.delete(`https://blogpessoalpedro.herokuapp.com/tema/${id}`, this.token)
   }
 
 
